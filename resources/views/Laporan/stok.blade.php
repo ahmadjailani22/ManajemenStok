8c431bc62786df7facfea4002b4ca65941647247
Nandarizalvy8 <nandaoppo439@gmail.com>
2026-05-03 14:58:30 +0700

    tampilan: tambah form pencarian dan filter kategori serta status stok

--- resources/views/Laporan/stok.blade.php
+++ resources/views/Laporan/stok.blade.php
@@ -52,6 +52,40 @@
                 </div>
             </div>
 
+            {{-- Form Search & Filter --}}
+            <div class="card">
+                <div class="card-body">
+                    <form method="GET" action="{{ route('laporan.stok') }}">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <input type="text" name="search" class="form-control"
+                                    placeholder="Cari kode / nama barang..."
+                                    value="{{ $search }}">
+                            </div>
+                            <div class="col-md-3">
+                                <select name="kategori" class="form-control">
+                                    <option value="">-- Semua Kategori --</option>
+                                    @foreach($kategoris as $kat)
+                                        <option value="{{ $kat->id_kategori }}"
+                                            {{ $filterKat == $kat->id_kategori ? 'selected' : '' }}>
+                                            {{ $kat->nama_kategori }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <select name="status" class="form-control">
+                                    <option value="">-- Semua Status --</option>
+                                    <option value="aman" {{ $filterStatus == 'aman' ? 'selected' : '' }}>Aman</option>
+                                    <option value="menipis" {{ $filterStatus == 'menipis' ? 'selected' : '' }}>Menipis</option>
+                                    <option value="habis" {{ $filterStatus == 'habis' ? 'selected' : '' }}>Habis</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
