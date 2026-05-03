e96df99477efaae6984bc8d959772cf2766935e4
Nandarizalvy8 <nandaoppo439@gmail.com>
2026-05-03 14:58:02 +0700

    tampilan: tambah kartu statistik total, aman, menipis, habis

--- resources/views/Laporan/stok.blade.php
+++ resources/views/Laporan/stok.blade.php
@@ -11,7 +11,47 @@
     </div>
     <div class="content">
         <div class="container-fluid">
-            {{-- konten akan ditambah bertahap --}}
+
+            {{-- Kartu Statistik --}}
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $totalBarang }}</h3>
+                            <p>Total Barang</p>
+                        </div>
+                        <div class="icon"><i class="fas fa-boxes"></i></div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $totalAman }}</h3>
+                            <p>Stok Aman</p>
+                        </div>
+                        <div class="icon"><i class="fas fa-check-circle"></i></div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $totalMenipis }}</h3>
+                            <p>Stok Menipis</p>
+                        </div>
+                        <div class="icon"><i class="fas fa-exclamation-triangle"></i></div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ $totalHabis }}</h3>
+                            <p>Stok Habis</p>
+                        </div>
+                        <div class="icon"><i class="fas fa-times-circle"></i></div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
