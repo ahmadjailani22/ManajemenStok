6e3c94dd4ce6907b3f5982758f86cd548b61413e
Nandarizalvy8 <nandaoppo439@gmail.com>
2026-05-03 14:41:54 +0700

    fitur: tambah import model dan library pada LaporanController

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class LaporanController extends Controller
 {
     //
